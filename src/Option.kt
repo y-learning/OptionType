1a3d282fc22bfd7927d5f9da9b970124f327bd47
Yahya <yahyatinani@gmail.com>
2020-07-18 09:51:21 +0100

    Implement map() function for Option

--- src/Option.kt
+++ src/Option.kt
@@ -3,6 +3,8 @@ import java.lang.RuntimeException
 sealed class Option<out T> {
     abstract fun isEmpty(): Boolean
 
+    abstract fun <U> map(f: (T) -> U): Option<U>
+
     fun getOrElse(default: () -> @UnsafeVariance T): T =
         when (this) {
             None -> default()
@@ -11,6 +13,8 @@ sealed class Option<out T> {
 
     object None : Option<Nothing>() {
         override fun isEmpty(): Boolean = true
+
+        override fun <U> map(f: (Nothing) -> U): Option<U> = None
 
         override fun toString(): String = "None"
 
@@ -21,6 +25,8 @@ sealed class Option<out T> {
 
     internal data class Some<T>(internal val value: T) : Option<T>() {
         override fun isEmpty(): Boolean = false
+
+        override fun <U> map(f: (T) -> U): Option<U> = Some(f(value))
     }
 
     companion object {
@@ -43,4 +49,7 @@ val max2 = max(listOf()).getOrElse { 0 }
 fun getDefault(): Int = throw RuntimeException()
 
 val max3 = max(listOf(4, 5, 62, 3, 7, 4, 5, 6)).getOrElse(::getDefault)
-val max4 = max(listOf()).getOrElse(::getDefault)
+// val max4 = max(listOf()).getOrElse(::getDefault)
+
+val max5 = max(listOf(5, 3, 8)).map { it + 1 }
+val max6 = max(listOf()).map { it + 1 }
