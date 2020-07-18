2cafe7a7aab565548231726b95fc04bc7c9e8983
Yahya <yahyatinani@gmail.com>
2020-07-18 09:31:22 +0100

    Make getOrElse() fun parameter lazy
    So it crashes only when the default is used

--- src/Option.kt
+++ src/Option.kt
@@ -1,9 +1,11 @@
+import java.lang.RuntimeException
+
 sealed class Option<out T> {
     abstract fun isEmpty(): Boolean
 
-    fun getOrElse(default: @UnsafeVariance T): T =
+    fun getOrElse(default: () -> @UnsafeVariance T): T =
         when (this) {
-            None -> default
+            None -> default()
             is Some -> this.value
         }
 
@@ -35,5 +37,10 @@ fun max(list: List<Int>): Option<Int> =
     if (list.isEmpty()) Option()
     else Option(list.max())
 
-val max1 = max(listOf(1, 2, 4, 5, 9, 5, 5, 2, 4, 3)).getOrElse(0)
-val max2 = max(listOf()).getOrElse(0)
+val max1 = max(listOf(1, 2, 4, 5, 9, 5, 5, 2, 4, 3)).getOrElse { 0 }
+val max2 = max(listOf()).getOrElse { 0 }
+
+fun getDefault(): Int = throw RuntimeException()
+
+val max3 = max(listOf(4, 5, 62, 3, 7, 4, 5, 6)).getOrElse(::getDefault)
+val max4 = max(listOf()).getOrElse(::getDefault)
