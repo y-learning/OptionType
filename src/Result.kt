cdeef625a58c8f693bde6fc5038960341f885728
Yahya <yahyatinani@gmail.com>
2020-07-20 15:19:56 +0100

    Implement map, flatMap, getOrElse, orElse forResult

--- src/Result.kt
+++ src/Result.kt
@@ -3,16 +3,60 @@ import java.lang.IllegalStateException
 import java.lang.RuntimeException
 
 sealed class Result<out A> : Serializable {
+    abstract fun <B> map(f: (A) -> B): Result<B>
+
+    abstract fun <B> flatMap(f: (A) -> Result<B>): Result<B>
+
+    fun getOrElse(defaultValue: @UnsafeVariance A): A =
+        when (this) {
+            is Failure -> defaultValue
+            is Success -> this.value
+        }
+
+    fun orElse(defaultValue: () -> Result<@UnsafeVariance A>): Result<A> =
+        when (this) {
+            is Success -> this
+            is Failure -> try {
+                defaultValue()
+            } catch (e: RuntimeException) {
+                Failure<A>(e)
+            } catch (e: Exception) {
+                Failure<A>(RuntimeException(e))
+            }
+        }
 
     internal
+
     class Failure<out A>(internal val exception: RuntimeException) :
         Result<A>() {
+
+        override fun <B> map(f: (A) -> B): Result<B> = Failure(exception)
+
+        override fun <B> flatMap(f: (A) -> Result<B>): Result<B> =
+            Failure(exception)
 
         override fun toString(): String = "Failure(exception=$exception)"
     }
 
     internal
     class Success<out A>(internal val value: A) : Result<A>() {
+
+        override fun <B> map(f: (A) -> B): Result<B> = try {
+            Success(f(value))
+        } catch (e: RuntimeException) {
+            Failure(e)
+        } catch (e: Exception) {
+            Failure(RuntimeException(e))
+        }
+
+        override fun <B> flatMap(f: (A) -> Result<B>): Result<B> = try {
+            f(value)
+        } catch (e: RuntimeException) {
+            Failure(e)
+        } catch (e: Exception) {
+            Failure(RuntimeException(e))
+        }
+
         override fun toString(): String = "Success(value=$value)"
     }
 
