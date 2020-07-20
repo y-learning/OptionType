780af0b5dcf827af6ae8d87c08abc0d0927ede65
Yahya <yahyatinani@gmail.com>
2020-07-20 11:24:23 +0100

    Implement flatMap() fun for Either

--- src/Either.kt
+++ src/Either.kt
@@ -1,19 +1,27 @@
-sealed class Either<out E, out A> {
+sealed class Either<E, out A> {
 
     abstract fun <B> map(f: (A) -> B): Either<E, B>
 
+    abstract fun <B> flatMap(f: (A) -> Either<E, B>): Either<E, B>
+
     internal
-    class Left<out E, out A>(private val value: E) : Either<E, A>() {
+    class Left<E, out A>(private val value: E) : Either<E, A>() {
         override fun toString(): String = "Left(value=$value)"
 
         override fun <B> map(f: (A) -> B): Either<E, B> = Left(value)
+
+        override fun <B> flatMap(f: (A) -> Either<E, B>): Either<E, B> =
+            Left(value)
     }
 
     internal
-    class Right<out E, out A>(private val value: A) : Either<E, A>() {
+    class Right<E, out A>(private val value: A) : Either<E, A>() {
         override fun toString(): String = "Right(value=$value)"
 
         override fun <B> map(f: (A) -> B): Either<E, B> = Right(f(value))
+
+        override fun <B> flatMap(f: (A) -> Either<E, B>): Either<E, B> =
+            f(value)
     }
 
     companion object {
