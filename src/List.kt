3292864f7aaa26547e498b5994caf19e521e84c2
Yahya <yahyatinani@gmail.com>
2020-07-22 15:46:56 +0100

    Rewrite pkg unzip() in term of instance unzip()

--- src/List.kt
+++ src/List.kt
@@ -51,11 +51,7 @@ fun <E, T, U> product(l1: List<E>, l2: List<T>, f: (E) -> (T) -> U): List<U> =
     l1.flatMap { e: E -> l2.map { t: T -> f(e)(t) } }
 
 fun <T, U> unzip(list: List<Pair<T, U>>): Pair<List<T>, List<U>> =
-    list.coFoldRight(Pair(List(), List())) { pair: Pair<T, U> ->
-        { acc: Pair<List<T>, List<U>> ->
-            Pair(acc.first.cons(pair.first), acc.second.cons(pair.second))
-        }
-    }
+    list.unzip { it }
 
 sealed class List<out E> {
     abstract val length: Int
